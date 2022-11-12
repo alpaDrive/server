732697592d95483bfb40ba818d94ac6f270059e1
vishalkrishnads <321vishalds@gmail.com>
2022-11-12 21:28:31 +0530

    Fixed #4

--- sockets/src/lib.rs
+++ sockets/src/lib.rs
@@ -12,6 +12,7 @@ pub mod sockets {
     pub struct Lobby {
         sessions: HashMap<String, Recipient<WsMessage>>,
         rooms: HashMap<String, HashSet<String>>,      //room id  to list of users id
+        admins: HashMap<String, String>
     }
 
     impl Default for Lobby {
@@ -19,6 +20,7 @@ pub mod sockets {
             Lobby {
                 sessions: HashMap::new(),
                 rooms: HashMap::new(),
+                admins: HashMap::new()
             }
         }
     }
@@ -55,22 +57,15 @@ pub mod sockets {
                 action: Action::Disconnect(code)
             });
         }
-        // helper to return the id of the vehicle in a room
-        fn get_vehicle_id(&mut self, room: String) -> Option<&String> {
-            if let Entry::Occupied(o) = self.rooms.entry(room) {
-                let mut temp = o.into_mut().iter().take(1);
-                temp.next()
-            } else { None }
-        }
         // send a message to the admin vehicle
         fn message_vehicle(&mut self, room: String, message: String) {
             let sessions = self.sessions.clone();
-            if let Some(room) = self.get_vehicle_id(room) {
-                if let Some(socket_recipient) = sessions.get(room) {
+            if let Entry::Occupied(admin) = self.admins.entry(room) {
+                if let Some(socket_recipient) = sessions.get(admin.get()) {
                     let _ = socket_recipient
                         .do_send(WsMessage {
                             message: message,
-                            id: room.to_string(),
+                            id: admin.get().to_string(),
                             action: Action::Send
                         });
                 }
@@ -82,7 +77,7 @@ pub mod sockets {
                 self_id.clone(),
                 addr.clone(),
             );
-            self.send_message(&"Connected", &self_id);
+            self.send_message(&format!("{} is your id", self_id), &self_id);
         }
     }
 
@@ -96,18 +91,21 @@ pub mod sockets {
 
         fn handle(&mut self, msg: Disconnect, _: &mut Context<Self>) {
             if self.sessions.remove(&msg.id).is_some() {
-                self.rooms
-                    .get(&msg.room_id)
-                    .unwrap()
-                    .iter()
-                    .filter(|conn_id| *conn_id.to_owned() != msg.id)
-                    .for_each(|user_id| self.send_message(&format!("{} disconnected.", &msg.id), user_id));
-                if let Some(lobby) = self.rooms.get_mut(&msg.room_id) {
-                    if lobby.len() > 1 {
-                        lobby.remove(&msg.id);
-                    } else {
-                        //only one in the lobby, remove it entirely
+                if let Entry::Occupied(admin) = self.admins.entry(msg.room_id.clone()) {
+                    if &msg.id == admin.get() {
+                        self.rooms
+                        .get(&msg.room_id)
+                        .unwrap()
+                        .iter()
+                        .filter(|conn_id| *conn_id.to_owned() != msg.id)
+                        .for_each(|user_id| self.send_disconnect("Vehicle left and the room is being closed", user_id, CloseCode::Normal));
                         self.rooms.remove(&msg.room_id);
+                        self.admins.remove(&msg.room_id);
+                    } else {
+                        self.message_vehicle(msg.room_id.clone(), format!("{} has disconnected", msg.id));
+                        if let Some(lobby) = self.rooms.get_mut(&msg.room_id) {
+                            lobby.remove(&msg.id);
+                        }
                     }
                 }
             }
@@ -128,7 +126,7 @@ pub mod sockets {
                             o.get_mut().insert(msg.self_id.clone());
                             self.insert(msg.self_id, msg.addr);
                         },
-                        Mode::Admin => self.send_disconnect_standalone("Vehicle with the specified ID has already connected.".to_string(), &msg.addr, msg.self_id, CloseCode::Policy),
+                        Mode::Admin => self.send_disconnect_standalone(String::from("Vehicle with the specified ID has already connected."), &msg.addr, msg.self_id, CloseCode::Policy),
                         Mode::Pair(message) => {
                             self.message_vehicle(msg.room_id, message.clone());
                             self.send_disconnect_standalone(message, &msg.addr, msg.self_id, CloseCode::Normal);
@@ -150,12 +148,12 @@ pub mod sockets {
                             let mut set = HashSet::new();
                             set.insert(msg.self_id.clone());
                             o.insert(set);
+                            self.admins.insert(msg.room_id, msg.self_id.clone());
                             self.insert(msg.self_id, msg.addr);
                         },
                         Mode::Pair(_) => self.send_disconnect_standalone(String::from("Vehicle isn't active at the moment. Try again later."), &msg.addr, msg.self_id, CloseCode::Protocol)
                     }
                 }
-
             }
         }
     }
